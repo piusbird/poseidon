32c4f8a280592676beb4321460d179c233473541
Matt Arnold <matt@thegnuguru.org>
2023-01-02 02:18:22 -0500

    Add selectable user agent support

--- main.go
+++ main.go
@@ -1,21 +1,60 @@
 package main
 
 import (
-	"net/http"
-	"os"
-	"net/url"
+	"bytes"
+	"compress/gzip"
+	"crypto/tls"
+	"errors"
+	"io"
 	"io/ioutil"
 	"log"
-	"crypto/tls"
-	"compress/gzip"
-	"bytes"
-	"io"
-	"strings"
+	"net/http"
+	"net/url"
+	"os"
 	"regexp"
+	"strings"
 )
 
+var default_agent = "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/W.X.Y.Z Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
 var ourProxy = "http://localhost:8090"
-func fetch(fetchurl string, muggle bool)  (*http.Response, error) {
+
+func postFormHandler(w http.ResponseWriter, r *http.Request) {
+	//if r.Method != http.MethodPost {
+
+	//http.Error(w, "Method not allowed"+r.Method, http.StatusInternalServerError)
+	//	return
+	//}
+	r.ParseForm()
+	log.Println(r.Form)
+	target_url := r.Form.Get("target_url")
+	ua := r.Form.Get("target_ua")
+
+	final := r.URL.Hostname() + "/" + target_url
+	cookie := http.Cookie{
+		Name:     "blueProxyUserAgent",
+		Value:    ua,
+		Path:     "/",
+		MaxAge:   3600,
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteLaxMode,
+	}
+	http.SetCookie(w, &cookie)
+	log.Println(final)
+	req, err := http.NewRequest("GET", final, nil)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+
+	}
+	req.Header.Add("X-Target-User-Agent", ua)
+	w.Header().Set("X-Target-User-Agent", ua)
+	http.Redirect(w, req, final, 302)
+
+}
+
+func fetch(fetchurl string, user_agent string) (*http.Response, error) {
 
 	proxyURL, err := url.Parse(ourProxy)
 	if err != nil {
@@ -23,7 +62,7 @@ func fetch(fetchurl string, muggle bool)  (*http.Response, error) {
 		return nil, err
 	}
 	transport := &http.Transport{
-		Proxy: http.ProxyURL(proxyURL),
+		Proxy:           http.ProxyURL(proxyURL),
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 	client := &http.Client{
@@ -34,12 +73,11 @@ func fetch(fetchurl string, muggle bool)  (*http.Response, error) {
 		log.Println(err)
 		return nil, err
 	}
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/W.X.Y.Z Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")
-	if muggle {
-		req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0")
+	req.Header.Set("User-Agent", default_agent)
+	if user_agent != "" {
+		req.Header.Set("User-Agent", user_agent)
 	}
-	
-	
+
 	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
 	req.Header.Set("Accept-Language", "en-US,en;q=0.9")
 	req.Header.Set("Accept-Encoding", "gzip")
@@ -57,39 +95,45 @@ func fetch(fetchurl string, muggle bool)  (*http.Response, error) {
 	}
 	return resp, err
 
-
 }
-
-var hello = []byte(
-	`<title> Blue DabaDeeDabaProxy </title>
-	<body><style>body { font-size: 2em; }</style>
-<p>hello there
-<form action="javascript:void(0);" onsubmit="go()">
-<p>URL: <input id="urlbox">
-<script>
-function go() {
-    var inp = document.getElementById("urlbox")
-    var val = inp.value
-    window.location.href = window.location.href + '/' +  val
-}
-</script>
-<button onclick="go()">GO</button>
-</form>
-`)
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.Header.Get("X-Target-User-Agent"))
 	if r.URL.Path == "/" {
-		w.Write(hello)
+		w.Write(template)
 		return
 	}
 	urlparts := strings.SplitN(r.URL.Path[1:], "/", 2)
 	if len(urlparts) < 2 {
 		return
 	}
+	agent := ""
 	remurl := urlparts[0] + "//" + urlparts[1]
-	resp, err := fetch(remurl, false)
+	cookie, err := r.Cookie("blueProxyUserAgent")
 	if err != nil {
-		log.Println(err)
+		switch {
+		case errors.Is(err, http.ErrNoCookie):
+			cookie := http.Cookie{
+				Name:     "blueProxyUserAgent",
+				Value:    default_agent,
+				Path:     "/",
+				MaxAge:   3600,
+				HttpOnly: true,
+				Secure:   true,
+				SameSite: http.SameSiteLaxMode,
+			}
+			http.SetCookie(w, &cookie)
+			http.Error(w, "Try Again", http.StatusBadRequest)
+		default:
+			log.Println(err)
+			http.Error(w, "server error", http.StatusInternalServerError)
+		}
+		return
+	}
+	agent = cookie.Value
+	resp, err := fetch(remurl, agent)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer resp.Body.Close()
@@ -99,7 +143,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 func muggleHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/muggle/" {
-		w.Write(hello)
+		w.Write(template)
 		log.Println("It Works!")
 		return
 	}
@@ -108,7 +152,6 @@ func muggleHandler(w http.ResponseWriter, r *http.Request) {
 
 	log.Println("Hello!")
 
-	
 	log.Println(res)
 
 	urlparts := strings.SplitN(res, "/", 2)
@@ -116,16 +159,15 @@ func muggleHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	remurl := urlparts[0] + "//" + urlparts[1]
-	resp, err := fetch(remurl, true)
+	resp, err := fetch(remurl, "")
 	if err != nil {
-		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer resp.Body.Close()
 	io.Copy(w, resp.Body)
 
 }
-
 
 func main() {
 	port := os.Getenv("PORT")
@@ -134,11 +176,27 @@ func main() {
 	}
 
 	mux := http.NewServeMux()
-
+	mux.HandleFunc("/redirect", postFormHandler)
+	mux.HandleFunc("/redirect/", postFormHandler)
 	mux.HandleFunc("/muggle/", muggleHandler)
 	mux.HandleFunc("/", indexHandler)
-	
+
 	http.ListenAndServe(":"+port, mux)
 }
 
+var template = []byte(
+	`<title> Blue DabaDeeDabaProxy </title>
+	<body><style>body { font-size: 2em; }</style>
+<p>hello there
+<form action="/redirect" method="post">
+<p>URL: <input name="target_url"> <br/>
+<label for="target_ua">Agent?</label>
 
+<select name="target_ua" id="cars">
+  <option value="Mozilla/5.0 (X11; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0">Desktop</option>
+  <option value="Twitterbot/1.0">Twitter</option>
+  <option value="Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/W.X.Y.Z Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)">Google</option>
+</select> <br><br>
+<input type="submit" value="Go">
+</form>
+`)
