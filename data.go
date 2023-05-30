f181295370a6e7d222c63c07adb3368047b3d672
Matt Arnold <matt@thegnuguru.org>
2023-05-29 22:45:18 -0400

    bump version

--- data.go
+++ data.go
@@ -1,6 +1,6 @@
 package main
 
-var version = "What the heck is gec"
+var version = "cf30f1f"
 var UserAgents = map[string]string{
 	"Desktop":          "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0",
 	"Googlebot Mobile": "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/W.X.Y.Z Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html",
