c3b15d728bfd9c7c0428c8c2016b9ebf892211da
Matt Arnold <matt@thegnuguru.org>
2023-05-31 09:20:21 -0400

    finally kill miniweb

--- appbuild.sh
+++ appbuild.sh
@@ -6,16 +6,6 @@ fi
 mkdir -pv $appRoot/src
 cd $appRoot/src
 echo $appRoot
-git clone https://git.piusbird.space/miniweb.git/
-cd miniweb
-make
-cp miniwebproxy $appRoot
-cp -r scripts/ $appRoot
-chmod +x sign.sh
-./sign.sh
-cp intercept* $appRoot
-cd ..
-sh -c miniweb/sign.sh
 git clone https://git.piusbird.space/poseidon.git/
 cd poseidon 
 go build
